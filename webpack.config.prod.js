1d86bd7ca626d0e9efe14fa5d5c0f489bc54f71b
Naoki Sawada <naoki.sawada@nextremer.com>
2017-06-10 23:20:04 +0900

    Add production build

---
+++ webpack.config.prod.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+
+module.exports = {
+  entry: __dirname + '/src/js/app.js',
+  output: {
+    path: __dirname + '/www/js',
+    publicPath: '/www/',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, "src/js"),
+      "node_modules"
+    ],
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: 'css-loader?modules,localIdentName="[name]-[local]-[hash:base64:6]"'
+        }),
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '../assets/css/style.css',
+      allChunks: true
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+  ]
+}
